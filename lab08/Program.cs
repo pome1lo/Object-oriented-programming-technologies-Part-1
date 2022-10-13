5a08a9bc28a67fc0b65eaceda1f7b325140bab23
Alexey <95231870+zephyrXXX@users.noreply.github.com>
2022-10-13 23:51:09 +0300

    Add lab08

---
+++ lab08/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab08
+{
+    internal class Program
+    {
+        public delegate void DelegateRemove(List<Programmer> MyList, int Index = 0);
+        public delegate void DelegateMutate(Programmer Item);
+
+        public static event DelegateRemove Remove;
+        public static event DelegateMutate Mutate;
+
+        static void Main(string[] args)
+        {
+            List<Programmer> MyList = new List<Programmer>();
+
+            MyList.Add(new Programmer("Anton "));
+            MyList.Add(new Programmer("Alex "));
+            MyList.Add(new Programmer("Stepan "));
+            MyList.Add(new Programmer("Victor "));
+
+            Programmer.Print(MyList);
+
+            //Programmer.Remove += Programmer.RemoveFoo;
+            //Programmer.Remove(MyList);
+            
+            Remove += Programmer.RemoveFoo;
+            Mutate += Programmer.MutateFoo;
+
+            Remove(MyList);
+            Programmer.Print(MyList);
+
+            Remove(MyList, 2);
+            Programmer.Print(MyList);
+
+            Mutate(MyList[0]);
+            Programmer.Print(MyList);
+
+            Mutate(MyList[0]);
+            Programmer.Print(MyList);
+
+            Mutate.Invoke(MyList[0]);
+            //Mutate(MyList[0]);
+            Programmer.Print(MyList);
+
+            Programmer.Сoncatenation("OOP ", "TOP )", Programmer.Add);
+
+            Console.WriteLine(Programmer.UpperOrLower("test"));
+
+            Console.WriteLine(Programmer.StringLen("TEST", Programmer.StrLen));
+
+
+            string startStr = "StRiNg TeST";
+            Console.WriteLine(startStr);
+            Action<string> CurrentString = (str) => Console.WriteLine(str);
+            Func<string, string> stringOperations = Programmer.RemoveExtraSpaces;
+
+            stringOperations += Programmer.ToDefaultCase;
+            CurrentString(startStr = stringOperations(startStr));
+
+            stringOperations += Programmer.ToUp;
+            CurrentString(startStr = stringOperations(startStr));
+
+            stringOperations += Programmer.StrLeng;
+            CurrentString(startStr = stringOperations(startStr));
+
+        }
+    }
+}
---
+++ lab08/Programmer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace lab08
+{
+
+    internal class Programmer
+    {
+        public Programmer()
+        {
+
+        }
+        public Programmer(string Name)
+        {
+            this.Name = Name;
+        }
+
+        // поля
+
+        //private ConsoleColor MutateColor = ConsoleColor.White;
+        private string Name = "there is no information.";
+        private string MutationStatus = "there is no information.";
+        public ConsoleColor MutateColor = ConsoleColor.Green;
+        private int ColorIndex = 1;
+
+        public int PropColorIndex
+        {
+            get { return ColorIndex; }
+            set { ColorIndex = value; }
+        }
+
+        public string PropMutationStatus
+        {
+            get { return MutationStatus; }
+            set { MutationStatus = value; }
+        }
+        public string PropName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public ConsoleColor PropMutateColor
+        {
+            get { return MutateColor; }
+            set { MutateColor = value; }
+        }
+
+        public static void MutateFoo(Programmer Item) => Item.PropColorIndex++;
+
+        public static void RemoveFoo(List<Programmer> MyList, int Index = 0) => MyList.RemoveAt(Index);
+
+        public static void Print(List<Programmer> MyList, ConsoleColor Color = ConsoleColor.White)
+        {
+            int i = 1;
+            foreach (var item in MyList)
+            {
+                Console.Write($"{i}) Programmer's name: {item.Name}\t\t|\t");
+
+                if (item.PropColorIndex == 2)
+                {
+                    item.PropMutateColor = ConsoleColor.White;
+                    item.MutationStatus = "There is an infection";
+                }
+                if (item.PropColorIndex == 3)
+                {
+                    item.PropMutateColor = ConsoleColor.Yellow;
+                    item.MutationStatus = "The mutation of the cells has begun";
+                }
+                if (item.PropColorIndex == 4)
+                {
+                    item.PropMutateColor = ConsoleColor.Red;
+                    item.MutationStatus = $"{item.Name}became a mutated monster!";
+                }
+
+                Console.ForegroundColor = item.MutateColor;
+                Console.WriteLine($"Mutation: {item.MutationStatus}");
+                Console.ResetColor();
+
+                i++;
+            }
+            Console.WriteLine();
+        }
+
+        public static void Сoncatenation(string a, string b, Action<string, string> conc) => conc(a, b);
+        public static void Add(string a, string b) => Console.WriteLine($"Concatenation: {a + b}");
+
+        public static Predicate<string> UpperOrLower = (string str) => (str.First() >= 1040 && str.First() <= 1071 || str.First() >= 65 && str.First() <= 90);
+
+        public static int StringLen(string str, Func<string, int> operation) => operation(str);
+        public static int StrLen(string str) => str.Length;
+
+
+        public static string RemoveExtraSpaces(string str) => Regex.Replace(str.Trim(), @"", " ");
+        public static string ToDefaultCase(string str) => $"{str.ToUpper().First()}{str.ToLower().Substring(1)}";
+        public static string ToUp(string str) => str.ToUpper();
+        public static string StrLeng(string str) => $"Number of characters {str.Length}";
+    }
+}
