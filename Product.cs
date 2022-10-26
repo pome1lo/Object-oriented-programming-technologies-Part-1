df05e7d610419632353258876ed8473c5b87f643
Alexey <95231870+zephyrXXX@users.noreply.github.com>
2022-10-26 18:43:39 +0300

    Add lab10

---
+++ Product.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab02
+{
+    partial class Product
+    {
+        public Product()
+        {
+            Name = "No information available.";
+            UPC = 0;
+            Producer = "No information available.";
+            Price = 0;
+            ShelfLife = "No information available.";
+            Quanity = 0;
+
+            Index++;
+        }
+        public Product(Guid Id)
+        {
+            this.Id = Id;
+            Name = "No information available.";
+            UPC = 0;
+            Producer = "No information available.";
+            Price = 0;
+            ShelfLife = "No information available.";
+            Quanity = 0;
+
+            Index++;
+        }
+        static Product()
+        {
+            Index = 0;
+        }
+        public Product(Guid Id, string Name, uint UPC, string Producer, float Price, string ShelfLife, int Quanity)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.UPC = UPC;
+            this.Producer = Producer;
+            this.Price = Price;
+            this.ShelfLife = ShelfLife;
+            this.Quanity = Quanity;
+
+            Index++;
+        }
+
+        static int Index { get; set; }
+
+        private Guid Id { get; }
+        private string Name;
+        private uint UPC;
+        private string Producer;
+        private float Price;
+        private string ShelfLife;
+        private int Quanity;
+        private const string TypeProduct = "Meal";
+
+
+
+
+        public string PropName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public uint PropUPC
+        {
+            get { return UPC; }
+            set { UPC = value; }
+        }
+        public string PropProducer
+        {
+            get { return Producer; }
+            set { Producer = value; }
+        }
+        public float PropPrice
+        {
+            get { return Price; }
+            set { Price = value; }
+        }
+        public string PropShelfLife
+        {
+            get { return ShelfLife; }
+            set { ShelfLife = value; }
+        }
+        public int PropQuanity
+        {
+            get { return Quanity; }
+            set { Quanity = value; }
+        }
+
+
+
+
+        /// <summary>
+        /// Printing a list item
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Id:\t\t{Id}\n" +
+                              $"Name:\t{Name}\n" +
+                              $"UPC:\t\t{UPC}\n" +
+                              $"Product Type: \t{TypeProduct} \n" +
+                              $"Manufacturer:\t{Producer}\n" +
+                              $"Price:\t\t{Price} $\n" +
+                              $"Expiration date:\t{ShelfLife}\n" +
+                              $"Quantity:\t{Quanity}\n");
+        }
+        /// <summary>
+        /// Printing the entire list of the list
+        /// </summary>
+        /// <param name="List"></param>
+        public static void PrintList(ref Product[] List)
+        {
+            for (int i = 0; i < List.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} position in the list:\n");
+                List[i].Print();
+            }
+        }
+        /// <summary>
+        /// Entering the list manually
+        /// </summary>
+        /// <param name="List"></param>
+        /// <returns></returns>
+        public static Product[] WriteListElement(ref Product[] List)
+        {
+            for (int i = 0; i < List.Length; i++)
+            {
+                Guid id = Guid.NewGuid();
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("UPC product: ");
+                uint upc = uint.Parse(Console.ReadLine());
+
+                Console.Write("Product Type: ");
+                string producer = Console.ReadLine();
+
+                Console.Write("Product price: ");
+                float price = float.Parse(Console.ReadLine());
+
+                Console.Write("Expiration date: ");
+                string shelfLife = Console.ReadLine();
+
+                Console.Write("Quanity: ");
+                int quanity = int.Parse(Console.ReadLine());
+
+                List[i] = new Product(id, name, upc, producer, price, shelfLife, quanity);
+                Console.WriteLine();
+            }
+            return List;
+        }
+        /// <summary>
+        /// Method describing the class
+        /// </summary>
+        public static void QuanityObject()
+        {
+            Console.WriteLine($"\nClass Product\nClass name: Product();\nCreated instances: {Index};\n"); ;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException();
+            }
+            Product temp = obj as Product;
+
+            return temp.Name == this.Name;
+        }
+        public override string ToString()
+        {
+            return String.Concat(Name, Producer, Price + " $", ShelfLife);
+        }
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 31 + Name.GetHashCode();
+            hash = hash * 31 + Price.GetHashCode();
+            hash = hash * 31 + Quanity.GetHashCode();
+            return hash;
+        }
+
+
+
+
+
+
+        
+        public static void PrintNameIf(List<Product> List, string str)
+        {
+            var newList = List.Where(x => x.Name == str);
+            Console.WriteLine('\n' + string.Join(Environment.NewLine, newList));
+        }
+        public static void PrintPriceIf(List<Product> MyList, string Str, float Price)
+        {
+            var newList = MyList.Where(x => x.Name == Str && x.Price < Price);
+            Console.WriteLine('\n' + string.Join(Environment.NewLine, newList));
+        }
+        public static void PriceOverHungred(List<Product> MyList)
+        {
+            var newList = MyList.Where(x => x.Price > 1F);
+            Console.WriteLine('\n' + string.Join(Environment.NewLine, "The number of items whose price is more than 100 ( or $ 1 ):" + newList.Count()));
+        }
+        public static void BestItem(List<Product> MyList)
+        {
+            var newList = MyList.OrderByDescending(x => x.Price);
+            Console.WriteLine('\n' + string.Join(Environment.NewLine, "Product with max. price: " + newList.First()));
+        }
+
+        public static void OrderItem(List<Product> MyList)
+        {
+            var newList1 = MyList.OrderByDescending(x => x.Producer);
+            Console.WriteLine("\nOrdered set of products by manufacturer:\n" + string.Join(Environment.NewLine, newList1));
+
+            var newList2 = MyList.OrderBy(x => x.PropQuanity);
+            Console.WriteLine("\nOrdered set of products by quantity: \n" + string.Join(Environment.NewLine, newList2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Year = {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June's",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
+            int i = 1;
+            var newYear = Year.Select(x => $"{i++}. {x}");
+            Console.WriteLine(string.Join(Environment.NewLine, newYear) + "\n");
+
+            // a query that selects a sequence of months with a string length equal to n
+            var newYear1 = Year.Aggregate("четы", (x, y) => y.Length == x.Length ? y : x);
+            Console.WriteLine(string.Join(Environment.NewLine, newYear1) + "\n");
+
+            // a request that returns only the summer and winter months
+            var newYear2 = from item in Year where (new[] { "January", "February", "June", "July", "August", "December" }.Contains(item)) select item;
+            Console.WriteLine(string.Join(Environment.NewLine, newYear2) + "\n");
+
+            // request to output months in alphabetical order
+            var newYear3 = Year.OrderBy(x => x);
+            Console.WriteLine(string.Join(Environment.NewLine, newYear3) + "\n");
+
+            // a query counting months containing the letter "u" and a name length of at least 4
+            var temp = Year.Where(x => x.Contains("u"));
+            var newYear4 = temp.Aggregate("12345", (x, y) => y.Length > x.Length ? y : x);
+            Console.WriteLine(string.Join(Environment.NewLine, newYear4) + "\n");
+
+            List<Product> MyList = new List<Product>();
+
+            MyList.Add(new Product(Guid.NewGuid(), "Chips ", 32312312, "Lays ", 1.4F, " 21.04.2023 ", 1));
+            MyList.Add(new Product(Guid.NewGuid(), "Dewdrop ", 3231897812, "MY ", 0.5F, " 21.01.2023 ", 1));
+            MyList.Add(new Product(Guid.NewGuid(), "Chips ", 34316319, "Chitos ", 0.9F, " 15.11.2022 ", 1));
+            MyList.Add(new Product(Guid.NewGuid(), "Chips ", 38642312, "Onega ", 1.1F, " 11.02.2023 ", 2));
+            MyList.Add(new Product(Guid.NewGuid(), "Popcorn ", 3231287912, "BUM ", 0.9F, " 11.01.2024 ", 1));
+            MyList.Add(new Product(Guid.NewGuid(), "Dumplings ", 323123902, "FRING", 2.8F, " 01.01.2184 ", 1));
+            MyList.Add(new Product(Guid.NewGuid(), "Cola ", 32334792, "Coca Kola ", 0.9F, " 11.09.2023 ", 3));
+            MyList.Add(new Product(Guid.NewGuid(), "Sprite ", 398657312, "Coca Kola ", 0.8F, " 11.09.2023 ", 3));
+            MyList.Add(new Product(Guid.NewGuid(), "Fanta ", 328792312, "Coca Kola ", 1.4F, " 11.09.2023 ", 5));
+            MyList.Add(new Product(Guid.NewGuid(), "Bonaqua", 32312432, "Water228 ", 0.8F, " 01.01.2024 ", 2));
+
+            Product.PrintNameIf(MyList, "Chips ");
+
+            Product.PrintPriceIf(MyList, "Chips ", 1.3F);
+
+            Product.PriceOverHungred(MyList);
+
+            Product.BestItem(MyList);
+
+            Product.OrderItem(MyList);
+
+            var MyListTest = new List<string>
+            {
+                "Alex",
+                "Vasia",
+                "Ludaed",
+                "Stepan",
+                "Sergey",
+                "Arseniy",
+                "Alexsandr"
+            };
+
+            var newList = MyListTest.Aggregate("Arseniy", (x, y) => x.Length > y.Length ? x : y).OrderBy(x => x).Select(x => $":  {x}").GroupBy(g => g).First();
+        }
+    }
+}
